0256653131937cf3c7c0ac380af6c7e334d4d05e
KyanBirb <97422254+KyanBirb@users.noreply.github.com>
2026-08-05 18:00:59 -0500

    Change sub-level pos pattern to get the local COM instead

--- common/src/main/java/dev/kyanbirb/world_casting/content/action/pattern/sub_level/OpGetSubLevelPos.kt
+++ common/src/main/java/dev/kyanbirb/world_casting/content/action/pattern/sub_level/OpGetSubLevelPos.kt
@@ -7,12 +7,15 @@ import at.petrak.hexcasting.api.casting.iota.Iota
 import dev.kyanbirb.world_casting.util.getFragment
 import dev.ryanhcode.sable.companion.math.JOMLConversion
 import dev.ryanhcode.sable.sublevel.ServerSubLevel
+import org.joml.Vector3d
 
 class OpGetSubLevelPos : ConstMediaAction {
     override val argc = 1
     override fun execute(args: List<Iota>, env: CastingEnvironment): List<Iota> {
         val fragment = args.getFragment(0, argc)
         val subLevel = fragment.getSubLevel(env.world) as ServerSubLevel
-        return JOMLConversion.toMojang(subLevel.logicalPose().position()).asActionResult
+        val centerOfMass = Vector3d(subLevel.logicalPose().position())
+        subLevel.logicalPose().transformPositionInverse(centerOfMass)
+        return JOMLConversion.toMojang(centerOfMass).asActionResult
     }
 }
